94069c4d9e55a48913b28898376e5be4d844b9a1
cranriar <69156990+cranriar@users.noreply.github.com>
2024-11-06 09:58:09 -0500

    Actualizar Product.php

--- src/Domain/Model/Product/Product.php
+++ src/Domain/Model/Product/Product.php
@@ -1,1 +1,26 @@
-producto
+<?php
+
+
+php id = $id;
+$this->nombre = $nombre; 
+$this->precio = $precio;
+$this->descripcion = $descripcion;
+$this->stock = $stock; 
+} 
+public function getId(): string { return $this->id; } 
+
+public function getNombre(): string { return $this->nombre; } 
+
+public function getPrecio(): float { return $this->precio; } 
+
+public function getDescripcion(): string { return $this->descripcion; } 
+
+public function getStock(): int { return $this->stock; } 
+
+public function actualizarStock(int $cantidad): void 
+{ 
+if ($cantidad < 0 && abs($cantidad) > $this->stock) 
+{ throw new \InvalidArgumentException("No hay suficiente stock para realizar esta operación."); } 
+$this->stock += $cantidad; } 
+
+public function estaDisponible(): bool { return $this->stock > 0; } }
